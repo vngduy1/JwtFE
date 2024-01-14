0c7b1eda8dbfcb9f8c33e0f2837d6f1e8609cbef
Duy <duyvu372001@gmail.com>
2024-01-15 00:03:21 +0900

    Register_complete

---
+++ src/routes/privateRoute.js
@@ -0,0 +1,21 @@
+import { useEffect } from 'react'
+import { Route } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
+
+const PrivateRoutes = (props) => {
+  let history = useHistory()
+  useEffect(() => {
+    let session = sessionStorage.getItem('account')
+    if (!session) {
+      history.push('/login')
+    }
+  }, [])
+
+  return (
+    <>
+      <Route path={props.path} component={props.component}></Route>
+    </>
+  )
+}
+
+export default PrivateRoutes
---
+++ src/services/userService.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+
+const registerNewUser = (email, phone, username, password) => {
+  return axios.post('http://localhost:8081/api/v1/register', {
+    email,
+    phone,
+    username,
+    password,
+  })
+}
+
+const loginUser = (valueLogin, password) => {
+  return axios.post('http://localhost:8081/api/v1/login', {
+    valueLogin,
+    password,
+  })
+}
+export { registerNewUser, loginUser }
