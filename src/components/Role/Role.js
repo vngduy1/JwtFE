76c84c2e15340c6ea271b9e1bdd511ad4921f03c
Duy <duyvu372001@gmail.com>
2024-01-25 00:43:56 +0900

    AddRoles

---
+++ src/components/Role/Role.js
@@ -0,0 +1,140 @@
+import { useState } from 'react'
+import _ from 'lodash'
+import { v4 as uuidv4 } from 'uuid'
+import { toast } from 'react-toastify'
+
+import './Role.scss'
+import { createRoles } from '../../services/roleService'
+
+const Role = (props) => {
+  const dataChildDefault = { url: '', description: '', isValidUrl: true }
+  const [listChild, setListChild] = useState({
+    child1: dataChildDefault,
+  })
+
+  const handleOnchangeInput = (name, value, key) => {
+    let _listChild = _.cloneDeep(listChild)
+    _listChild[key][name] = value
+    if (value && name === 'url') {
+      _listChild[key]['isValidUrl'] = true
+    }
+    setListChild(_listChild)
+  }
+  const handleAddNewInput = () => {
+    let _listChild = _.cloneDeep(listChild)
+    _listChild[`child-${uuidv4()}`] = dataChildDefault
+    setListChild(_listChild)
+  }
+
+  const handleDeleteInput = (key) => {
+    let _listChild = _.cloneDeep(listChild)
+    delete _listChild[key]
+    setListChild(_listChild)
+  }
+
+  const buildDataToPersist = () => {
+    let _listChild = _.cloneDeep(listChild)
+    let result = []
+    Object.entries(_listChild).map(([key, child], index) => {
+      result.push({
+        url: child.url,
+        description: child.description,
+      })
+      return null
+    })
+    return result
+  }
+
+  const handleSave = async () => {
+    let invalidObj = Object.entries(listChild).find(([key, child, index]) => {
+      return child && !child.url
+    })
+
+    if (!invalidObj) {
+      //call api
+      let data = buildDataToPersist()
+      let res = await createRoles(data)
+      if (res && res.EC === 0) {
+        toast.success(res.EM)
+      }
+    } else {
+      toast.error('input Url isEmpty')
+      let _listChild = _.cloneDeep(listChild)
+      const key = invalidObj[0]
+      _listChild[key]['isValidUrl'] = false
+      setListChild(_listChild)
+    }
+  }
+
+  return (
+    <div className="role-container">
+      <div className="container">
+        <div className="mt-3">
+          <div className="title-role">
+            <h4>Add a new role ...</h4>
+            <div className="role-parents">
+              {Object.entries(listChild).map(([key, child], index) => {
+                return (
+                  <div className="row role-child" key={`child-${key}`}>
+                    <div className={`col-12 col-sm-5 mt-3 form-group ${key}`}>
+                      <label>URL:</label>
+                      <input
+                        type="text"
+                        className={
+                          child.isValidUrl
+                            ? 'form-control'
+                            : 'form-control is-invalid'
+                        }
+                        value={child.url}
+                        onChange={(event) =>
+                          handleOnchangeInput('url', event.target.value, key)
+                        }
+                      />
+                    </div>
+                    <div className="col-12 col-sm-5 mt-3 form-group">
+                      <label>Description</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        value={child.description}
+                        onChange={(event) =>
+                          handleOnchangeInput(
+                            'description',
+                            event.target.value,
+                            key,
+                          )
+                        }
+                      />
+                    </div>
+                    <div className="col-2 mt-5 actions">
+                      <i
+                        className="fa fa-plus-circle add "
+                        onClick={() => handleAddNewInput()}
+                      ></i>
+                      {index >= 1 && (
+                        <i
+                          className="fa fa-minus-square-o delete"
+                          onClick={() => handleDeleteInput(key)}
+                        ></i>
+                      )}
+                    </div>
+                  </div>
+                )
+              })}
+              <div>
+                <button
+                  className="btn btn-warning mt-3"
+                  onClick={() => handleSave()}
+                >
+                  保存
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Role
--- src/routes/appRoute.js
+++ src/routes/appRoute.js
@@ -1,37 +1,41 @@
+import { Switch, Route } from 'react-router-dom'
+
 import Login from '../components/login/login'
 import Register from '../components/register/register'
+import Role from '../components/Role/Role'
 import Users from '../components/manageUsers/Users'
-import { Switch, Route } from 'react-router-dom'
 import PrivateRoutes from './privateRoute'
+
 const AppRoutes = () => {
-    /**
-     * quản lý theo đường link
-     */
-    const Project = () => {
-        return (
-            <>
-                <span>projects</span>
-            </>
-        )
-    }
+  /**
+   * quản lý theo đường link
+   */
+  const Project = () => {
     return (
-        <>
-            <Switch>
-                <PrivateRoutes path="/users" component={Users} />
-                <PrivateRoutes path="/projects" component={Project} />
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route path="/register">
-                    <Register />
-                </Route>
-                <Route path="/" exact>
-                    Home
-                </Route>
-                <Route path="*">404 not found</Route>
-            </Switch>
-        </>
+      <>
+        <span>projects</span>
+      </>
     )
+  }
+  return (
+    <>
+      <Switch>
+        <PrivateRoutes path="/users" component={Users} />
+        <PrivateRoutes path="/projects" component={Project} />
+        <PrivateRoutes path="/roles" component={Role} />
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/" exact>
+          Home
+        </Route>
+        <Route path="*">404 not found</Route>
+      </Switch>
+    </>
+  )
 }
 
 export default AppRoutes
